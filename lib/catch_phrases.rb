91d559fd9e8b998d40c488ad3f4e4ab8e3733fe1
Courtney DeRiggs <courtney.deriggs@gmail.com>
2019-10-27 17:44:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,20 +1,19 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  end
+end
   
 def toadstool
   phrase = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts phrase
-  end
+end
 
 def link
   phrase = "It's Dangerous To Go Alone! Take This." 
   puts phrase
-  end
+end
 
 def any_phrase
     phrase = "Do A Barrel Roll!"
     puts phrase
-  end
 end
